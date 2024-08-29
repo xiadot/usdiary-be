2fcf9046085d922169dbe95e5f22091229a378da
SIA <kimsiah08@naver.com>
2024-08-30 02:23:05 +0900

    ✨ Feat: 댓글 작성, 수정 기능 구현

--- controllers/comment.js
+++ controllers/comment.js
@@ -2,6 +2,67 @@ const Comment = require('../models/comment');
 const Diary = require('../models/diary');
 const User = require('../models/user');
 
+// 댓글 작성
+exports.createComment = async (req, res) => {
+    try {
+        const diaryId = req.params.diary_id;
+        const { content } = req.body;
+        const userId = req.user.id; // 인증된 사용자의 ID를 가져오기
+
+        // 해당 일기가 존재하는지 확인
+        const diary = await Diary.findByPk(diaryId);
+        if (!diary) {
+            return res.status(404).json({ message: 'Diary not found' });
+        }
+
+        // 새로운 댓글 생성
+        const comment = await Comment.create({
+            content: content,
+            user_id: userId,
+            diary_id: diaryId
+        });
+
+        // 생성된 댓글 반환
+        res.status(201).json(comment);
+    } catch (error) {
+        console.error('Error creating comment:', error);
+        res.status(500).json({ message: 'Server error', error });
+    }
+};
+
+// 댓글 수정
+exports.updateComment = async (req, res) => {
+    try {
+        const { diaryId, commentId } = req.params;
+        const { content } = req.body;
+        const userId = req.user.id; // 인증된 사용자의 ID를 가져오기
+
+        // 해당 일기가 존재하는지 확인
+        const diary = await Diary.findByPk(diaryId);
+        if (!diary) {
+            return res.status(404).json({ message: 'Diary not found' });
+        }
+
+        // 수정할 댓글이 존재하는지 확인
+        const comment = await Comment.findOne({
+            where: { comment_id: commentId, diary_id: diaryId, user_id: userId }
+        });
+
+        if (!comment) {
+            return res.status(404).json({ message: 'Comment not found or you do not have permission to edit this comment' });
+        }
+
+        // 댓글 내용 수정
+        comment.content = content;
+        await comment.save();
+
+        res.status(200).json(comment);
+    } catch (error) {
+        console.error('Error updating comment:', error);
+        res.status(500).json({ message: 'Server error', error });
+    }
+};
+
 // 댓글 조회
 exports.renderComments = async (req, res) => {
   try {
--- routes/comment.js
+++ routes/comment.js
@@ -5,6 +5,12 @@ const { renderComments, deleteComment } = require('../controllers/comment');
 //댓글 조회 - 특정 일기에 대한 댓글 조회
 router.get('/:diary_id/comments', renderComments);
 
+// 댓글 작성 - 특정 일기에 댓글 작성
+router.post('/:diary_id/comments', createComment);
+
+// 댓글 수정 - 특정 댓글 수정
+router.put('/:diaryId/comments/:commentId', updateComment);
+
 //댓글 삭제 - 특정 댓글 삭제
 router.delete('/comments/:comment_id', deleteComment);
 
